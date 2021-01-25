ea95d2f53dc06574f4cdcfc8c6d2496de33786b7
Oguzhan Alagoz <ogzogz88@gmail.com>
2021-01-26 01:39:58 +0300

    Add inline handler to Item component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,26 @@
 import React, { useState, useEffect, useRef } from "react";
 import './App.css';
 
+const initialStories = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
+
+
 // using custom hook, name is useSemiPersistentState just because it uses localStorage, and deleting localStorage may change the state
 // custom hook name convention: use + UpperCase name
 // return values must be an array of values
@@ -17,24 +37,14 @@ const useSemiPersistentState = (key, initialState) => {
 
 const App = () => {
   const [searchTerm, setSearchTerm] = useSemiPersistentState("search", "React");
-  const stories = [
-    {
-      title: 'React',
-      url: 'https://reactjs.org/',
-      author: 'Jordan Walke',
-      num_comments: 3,
-      points: 4,
-      objectID: 0,
-    },
-    {
-      title: 'Redux',
-      url: 'https://redux.js.org/',
-      author: 'Dan Abramov, Andrew Clark',
-      num_comments: 2,
-      points: 5,
-      objectID: 1,
-    },
-  ];
+  const [stories, setStories] = React.useState(initialStories);
+
+  const handleRemoveStory = (item) => {
+    const newStories = stories.filter(
+      story => item.objectID !== story.objectID
+    );
+    setStories(newStories);
+  }
 
   const handleSearch = (event) => {
     setSearchTerm(event.target.value);
@@ -55,25 +65,48 @@ const App = () => {
         <strong>Search:</strong>
       </InputWithLabel>
       <hr />
-      <List list={searchedStories} />
+      <List list={searchedStories} onRemoveItem={handleRemoveStory} />
     </div>
   );
 }
-const List = ({ list }) => {
+const List = ({ list, onRemoveItem }) => {
   //using rest and spread operators together (first 3dots is rest, second 3dots is spread operator)
-  return list.map(({ objectID, ...item }) => <Item key={objectID} {...item} />);
+  //return list.map(({ objectID, ...item }) => <Item key={objectID} {...item} onRemoveItem={onRemoveItem} />);
+  return list.map(item => (
+    <Item
+      key={item.objectID}
+      item={item}
+      onRemoveItem={onRemoveItem}
+    />
+  ))
 }
 
-const Item = ({ title, url, author, num_comments, points }) => (
-  <div>
-    <span>
-      <a href={url}>{title}</a>
-    </span>
-    <span>{author}</span>
-    <span>{num_comments}</span>
-    <span>{points}</span>
-  </div>
-);
+const Item = ({ item, onRemoveItem }) => {
+  //executing item remove in the classical way
+  const handleRemoveItem = () => onRemoveItem(item);
+
+  return (
+    <div>
+      <span>
+        <a href={item.url}>{item.title}</a>
+      </span>
+      <span>{item.author}</span>
+      <span>{item.num_comments}</span>
+      <span>{item.points}</span>
+      <span>
+        {/* executing item remove in the classical way
+      <button type="button" onClick={handleRemoveItem}> */}
+
+        {/* using inline handler by the help of arrow functions */}
+        {/* avoid complex logic here!, one function to execute is enough */}
+        <button type="button" onClick={() => handleRemoveItem(item)}>
+          Dismiss
+        </button>
+      </span>
+
+    </div>
+  );
+};
 
 const InputWithLabel = ({ id, value, type = 'text', onInputChange, isFocused, children }) => {
   const inputRef = useRef();
