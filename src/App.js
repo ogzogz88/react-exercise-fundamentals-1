1fc504e834d2a37fb722e87883617d95fd1a7ba4
Oguzhan Alagoz <ogzogz88@gmail.com>
2021-01-30 17:40:18 +0300

    Change client-side data fetching to server-side data fetching, by implementing dynamic searchTerm

--- src/App.js
+++ src/App.js
@@ -60,8 +60,9 @@ const App = () => {
 
   // simulate async data fetching
   useEffect(() => {
+    if (!searchTerm) return;
     dispatchStories({ type: "STORIES_FETCH_INIT" });
-    fetch(`${API_ENDPOINT}react`)
+    fetch(`${API_ENDPOINT}${searchTerm}`)
       .then(response => response.json())
       .then(result => {
         dispatchStories({
@@ -72,7 +73,7 @@ const App = () => {
       .catch(() => dispatchStories({ type: 'STORIES_FETCH_FAILURE' })
       );
 
-  }, []);
+  }, [searchTerm]);
 
   const handleRemoveStory = (item) => {
     dispatchStories({
@@ -84,9 +85,6 @@ const App = () => {
   const handleSearch = (event) => {
     setSearchTerm(event.target.value);
   }
-  const searchedStories = stories.data.filter(story => {
-    return story.title.toLowerCase().includes(searchTerm.toLowerCase());
-  });
   return (
     <div>
       <h1>Hacker Stories</h1>
@@ -106,7 +104,7 @@ const App = () => {
         (<p>Loading...</p>) :
         (
           <List
-            list={searchedStories}
+            list={stories.data}
             onRemoveItem={handleRemoveStory}
           />
         )
