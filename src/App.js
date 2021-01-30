2ad50fb967cafa85c1f1e91b4f6cadd72d589d6c
Oguzhan Alagoz <ogzogz88@gmail.com>
2021-01-30 17:29:00 +0300

    Change getAsyncStories to fetch, to get real data

--- src/App.js
+++ src/App.js
@@ -75,6 +75,7 @@ const useSemiPersistentState = (key, initialState) => {
   return [value, setValue];
 }
 
+const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?query=';
 
 const App = () => {
   const [searchTerm, setSearchTerm] = useSemiPersistentState("search", "React");
@@ -88,11 +89,12 @@ const App = () => {
   // simulate async data fetching
   useEffect(() => {
     dispatchStories({ type: "STORIES_FETCH_INIT" });
-    getAsyncStories()
+    fetch(`${API_ENDPOINT}react`)
+      .then(response => response.json())
       .then(result => {
         dispatchStories({
           type: "STORIES_FETCH_SUCCESS",
-          payload: result.data.stories,
+          payload: result.hits,
         });
       })
       .catch(() => dispatchStories({ type: 'STORIES_FETCH_FAILURE' })
